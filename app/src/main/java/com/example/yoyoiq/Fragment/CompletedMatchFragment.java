90a5119a4a8beed35c1d57e8237317cdba84e7bb
Mt Software Solutions <95462784+mtteam21@users.noreply.github.com>
2022-07-11 18:33:54 +0530

    Work on MyMatchesFragment..

---
+++ app/src/main/java/com/example/yoyoiq/Fragment/CompletedMatchFragment.java
@@ -0,0 +1,45 @@
+package com.example.yoyoiq.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.yoyoiq.R;
+
+public class CompletedMatchFragment extends Fragment {
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    private String mParam1;
+    private String mParam2;
+
+    public CompletedMatchFragment() {
+        // Required empty public constructor
+    }
+
+    public static CompletedMatchFragment newInstance(String param1, String param2) {
+        CompletedMatchFragment fragment = new CompletedMatchFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_completed_match, container, false);
+    }
+}
--- app/src/main/java/com/example/yoyoiq/MyMatchesFragment.java
+++ app/src/main/java/com/example/yoyoiq/MyMatchesFragment.java
@@ -1,15 +1,16 @@
 package com.example.yoyoiq;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
 
 public class MyMatchesFragment extends Fragment {
-
+    TextView textView, upcoming, live, completed;
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
@@ -41,6 +42,42 @@ public class MyMatchesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_my_matches, container, false);
+        View root = inflater.inflate(R.layout.fragment_my_matches, container, false);
+        textView = root.findViewById(R.id.upcomingMatch);
+        live = root.findViewById(R.id.live);
+        upcoming = root.findViewById(R.id.upcoming);
+        completed = root.findViewById(R.id.completed);
+
+        upcoming.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setVisibility(View.VISIBLE);
+            }
+        });
+
+        live.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setVisibility(View.VISIBLE);
+            }
+        });
+
+        completed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return root;
     }
 }
