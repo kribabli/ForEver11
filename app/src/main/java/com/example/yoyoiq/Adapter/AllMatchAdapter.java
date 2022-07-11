8f293b7bd723fb72303ce224a1a4b8020aff3e40
Mt Software Solutions <95462784+mtteam21@users.noreply.github.com>
2022-07-11 15:10:53 +0530

    work on ContestActivity.java

--- app/src/main/java/com/example/yoyoiq/Adapter/AllMatchAdapter.java
+++ app/src/main/java/com/example/yoyoiq/Adapter/AllMatchAdapter.java
@@ -1,6 +1,8 @@
 package com.example.yoyoiq.Adapter;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,9 +10,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.yoyoiq.ContestActivity;
 import com.example.yoyoiq.Modal.TotalHomeData;
 import com.example.yoyoiq.R;
 
@@ -33,8 +37,20 @@ public class AllMatchAdapter extends RecyclerView.Adapter<AllMatchAdapter.MyView
     }
 
     @Override
-    public void onBindViewHolder(@NonNull AllMatchAdapter.MyViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull AllMatchAdapter.MyViewHolder holder, @SuppressLint("RecyclerView") int position) {
         TotalHomeData listData = list.get(position);
+
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, ContestActivity.class);
+                intent.putExtra("shortNameA", listData.getShort_name_a());
+                intent.putExtra("shortNameB", listData.getShort_name_b());
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(intent);
+            }
+        });
+
         holder.textViewTitle.setText(list.get(position).getTitle());
 
         holder.matchATv.setText(listData.getName_a());
@@ -58,6 +74,7 @@ public class AllMatchAdapter extends RecyclerView.Adapter<AllMatchAdapter.MyView
     public class MyViewHolder extends RecyclerView.ViewHolder {
         TextView textViewTitle, matchATv, matchBTv, shortNameA, shortNameB;
         ImageView matchAImage, matchBImage;
+        CardView cardView;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -68,6 +85,7 @@ public class AllMatchAdapter extends RecyclerView.Adapter<AllMatchAdapter.MyView
             shortNameB = itemView.findViewById(R.id.shortNameB);
             matchAImage = itemView.findViewById(R.id.matchaImage);
             matchBImage = itemView.findViewById(R.id.matchbImage);
+            cardView = itemView.findViewById(R.id.cardViewMatchList);
         }
     }
 }
--- app/src/main/java/com/example/yoyoiq/ContestActivity.java
+++ app/src/main/java/com/example/yoyoiq/ContestActivity.java
@@ -1,14 +1,45 @@
 package com.example.yoyoiq;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.yoyoiq.WalletPackage.AddCash;
 
 public class ContestActivity extends AppCompatActivity {
+    TextView walletTV, backPress, matchATv, matchBTv;
+    String matchA = "", matchB = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.contest_list);
+        setContentView(R.layout.activity_contest);
+        walletTV = findViewById(R.id.walletTV);
+        backPress = findViewById(R.id.backPress);
+        matchATv = findViewById(R.id.matchATv);
+        matchBTv = findViewById(R.id.matchBTv);
+
+        matchA = getIntent().getStringExtra("shortNameA");
+        matchB = getIntent().getStringExtra("shortNameB");
+        matchATv.setText(matchA);
+        matchBTv.setText(matchB);
+
+        walletTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ContestActivity.this, AddCash.class);
+                startActivity(intent);
+            }
+        });
+
+        backPress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
     }
 }
