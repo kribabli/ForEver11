ee2566fd6c0e17737f9f791ebe1aec5f5b100f51
Mt Software Solutions <95462784+mtteam21@users.noreply.github.com>
2022-07-13 14:47:56 +0530

    work on PaymentOptions.java

--- app/src/main/java/com/example/yoyoiq/PaymentOptions.java
+++ app/src/main/java/com/example/yoyoiq/PaymentOptions.java
@@ -1,26 +1,31 @@
 package com.example.yoyoiq;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class PaymentOptions extends AppCompatActivity {
-    TextView backPress;
+    TextView backPress, passAmount;
+    String amount = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_options);
 
-        backPress=findViewById(R.id.backPress);
+        passAmount = findViewById(R.id.passAmount);
+        amount = getIntent().getStringExtra("amount");
+        passAmount.setText(amount);
 
+        backPress = findViewById(R.id.backPress);
         backPress.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 onBackPressed();
             }
         });
+
     }
 }
--- app/src/main/java/com/example/yoyoiq/WalletPackage/AddCash.java
+++ app/src/main/java/com/example/yoyoiq/WalletPackage/AddCash.java
@@ -3,27 +3,39 @@ package com.example.yoyoiq.WalletPackage;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.yoyoiq.NotificationActivity;
-import com.example.yoyoiq.PaymentOptions;
 import com.example.yoyoiq.R;
 import com.razorpay.Checkout;
+import com.razorpay.PaymentResultListener;
 
-public class AddCash extends AppCompatActivity {
-    TextView addCash, backPress, myRecentPay, KYCDetails,notification;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class AddCash extends AppCompatActivity implements PaymentResultListener {
+    TextView addCash, backPress, myRecentPay, KYCDetails, notification;
+    EditText amount;
+    String amount1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_cash2);
 
+        addCash = findViewById(R.id.addCash);
+        amount = findViewById(R.id.amount);
+        amount1 = amount.getText().toString();
+        addCash.setText(amount1);
+
         Checkout.preload(getApplicationContext());
 
-        notification=findViewById(R.id.notification);
-        addCash = findViewById(R.id.addCash);
+        notification = findViewById(R.id.notification);
         backPress = findViewById(R.id.backPress);
         myRecentPay = findViewById(R.id.myRecentPay);
         KYCDetails = findViewById(R.id.KYCDetails);
@@ -44,11 +56,32 @@ public class AddCash extends AppCompatActivity {
             }
         });
 
+        String sAmount = "100";
+        int amount = Math.round(Float.parseFloat(sAmount) * 100);
+
         addCash.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(AddCash.this, PaymentOptions.class);
-                startActivity(intent);
+//                Intent intent = new Intent(AddCash.this, PaymentOptions.class);
+//                intent.putExtra("amount", amount.getText().toString());
+//                startActivity(intent);
+                Checkout checkout = new Checkout();
+                checkout.setKeyID("ZOWulQ5AH9ssOxyn1mAPeqIn");
+                checkout.setImage(R.drawable.facebook);
+
+                JSONObject jsonObject = new JSONObject();
+                try {
+                    jsonObject.put("name", "YoYoIq");
+                    jsonObject.put("description", "Test Payment");
+                    jsonObject.put("theme.color", "#0093DD");
+                    jsonObject.put("currency", "INR");
+                    jsonObject.put("amount", amount);
+                    jsonObject.put("contact", "6201623832");
+                    jsonObject.put("meratemplate", "mtteam.suraj@gmail.com");
+                    checkout.open(AddCash.this, jsonObject);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
         });
 
@@ -66,5 +99,18 @@ public class AddCash extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public void onPaymentSuccess(String s) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Payment Success..");
+        builder.setMessage(s);
+        builder.show();
+    }
+
+    @Override
+    public void onPaymentError(int i, String s) {
+        Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
     }
 }
