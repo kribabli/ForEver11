9a630f5f89c7467f6d55657e827f50d08ecd3a97
Mt Software Solutions <95462784+mtteam21@users.noreply.github.com>
2022-08-16 19:07:23 +0530

    ProfileActivity.java..

--- app/src/main/java/com/example/yoyoiq/ProfileActivity.java
+++ app/src/main/java/com/example/yoyoiq/ProfileActivity.java
@@ -1,30 +1,72 @@
 package com.example.yoyoiq;
 
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
+import com.example.yoyoiq.common.HelperData;
 import com.example.yoyoiq.common.SharedPrefManager;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileActivity extends AppCompatActivity {
-    TextView backPress;
+    TextView backPress, userName, skillScore, share;
     SharedPrefManager sharedPrefManager;
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+    CircleImageView userProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        initMethod();
+        setAction();
+        setProfileDetails();
+    }
+
+    private void initMethod() {
         backPress = findViewById(R.id.backPress);
-
+        userName = findViewById(R.id.userName);
+        skillScore = findViewById(R.id.skillScore);
+        userProfile = findViewById(R.id.profilePic);
+        share = findViewById(R.id.share);
         sharedPrefManager = new SharedPrefManager(getApplicationContext());
+    }
 
-        backPress.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                onBackPressed();
-            }
-        });
+    private void setAction() {
+        backPress.setOnClickListener(view -> onBackPressed());
+
+        share.setOnClickListener(v -> share());
+    }
+
+    private void share() {
+
+    }
+
+    private void setProfileDetails() {
+        userName.setText(HelperData.UserName);
+
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc = GoogleSignIn.getClient(this, gso);
+        GoogleSignInAccount googleSignInAccount = GoogleSignIn.getLastSignedInAccount(this);
+        if (googleSignInAccount != null) {
+            String UserName = googleSignInAccount.getDisplayName();
+            String userEmail = googleSignInAccount.getEmail();
+            Uri photoUrl = googleSignInAccount.getPhotoUrl();
+            String id = googleSignInAccount.getId();
+            userName.setText(UserName);
+
+            Glide.with(this)
+                    .load(photoUrl)
+                    .into(userProfile);
+        }
     }
 }
