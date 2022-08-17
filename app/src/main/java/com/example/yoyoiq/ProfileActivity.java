b19e203116618c9c07a5dd8cf813892bc6470049
Mt Software Solutions <95462784+mtteam21@users.noreply.github.com>
2022-08-17 12:12:55 +0530

    ProfileActivity.java..

--- app/src/main/java/com/example/yoyoiq/ProfileActivity.java
+++ app/src/main/java/com/example/yoyoiq/ProfileActivity.java
@@ -1,9 +1,15 @@
 package com.example.yoyoiq;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
@@ -44,11 +50,41 @@ public class ProfileActivity extends AppCompatActivity {
     private void setAction() {
         backPress.setOnClickListener(view -> onBackPressed());
 
-        share.setOnClickListener(v -> share());
+        //Profile Share-----------------------------------------
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                    shareIntent.setType("text/plain");
+                    shareIntent.putExtra(Intent.EXTRA_SUBJECT, "YoYoIQ");
+                    String shareMessage = "\nLet me recommend you this application\n\n";
+                    shareMessage = shareMessage + "https://play.google.com/store/apps/details?id=" + BuildConfig.APPLICATION_ID + "\n\n";
+                    shareIntent.putExtra(Intent.EXTRA_TEXT, shareMessage);
+                    startActivity(Intent.createChooser(shareIntent, "choose one"));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
-    private void share() {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.profile_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.myInfo:
+                return true;
+            case R.id.searchProfile:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void setProfileDetails() {
