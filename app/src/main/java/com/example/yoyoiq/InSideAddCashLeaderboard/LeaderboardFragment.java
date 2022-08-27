c0948ca0ce2af9be5b53fd9fa5236e3b9e223aa9
Mt Software Solutions <95462784+mtteam21@users.noreply.github.com>
2022-08-27 12:53:46 +0530

    main/AndroidManifest.xml.

--- app/src/main/java/com/example/yoyoiq/InSideAddCashLeaderboard/LeaderboardFragment.java
+++ app/src/main/java/com/example/yoyoiq/InSideAddCashLeaderboard/LeaderboardFragment.java
@@ -1,116 +1,95 @@
-package com.example.yoyoiq.InSideAddCashLeaderboard;
+com.example.yoyoiq.OnlyTeamPreView.OnlyTeamPreview"
+            android:exported="false" />
+        <activity
+            android:name="com.example.yoyoiq.ChatFirebase.ChatActivity"
+            android:exported="true" />
+        <activity
+            android:name="com.example.yoyoiq.ChatFirebase.DirectChatActivity"
+            android:exported="true" />
+        <activity
+            android:name="com.example.yoyoiq.KYC.ShowKYCDetails"
+            android:exported="false" />
+        <activity
+            android:name="com.example.yoyoiq.CreateTeamActivity"
+            android:exported="false" />
+        <activity
+            android:name="com.example.yoyoiq.PrivacyPolicy.AboutUsActivity"
+            android:exported="false" />
+        <activity
+            android:name="com.example.yoyoiq.ContestActivity"
+            android:exported="true" />
+        <activity
+            android:name="com.example.yoyoiq.KYC.KYCActivity"
+            android:exported="false" />
+        <activity
+            android:name="com.example.yoyoiq.RegisterDetails"
+            android:exported="false" />
+        <activity
+            android:name="com.example.yoyoiq.RegisterActivity"
+            android:exported="true" />
+        <activity
+            android:name="com.example.yoyoiq.FrontActivity"
+            android:exported="true" />
+        <activity
+            android:name="com.example.yoyoiq.SearchActivity"
+            android:exported="false" />
+        <activity
+            android:name="com.example.yoyoiq.SplashActivity"
+            android:exported="true" >
+            <intent-filter>
+                <action android:name="android.intent.action.MAIN" />
 
-import android.content.Context;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
-
-import com.example.yoyoiq.R;
-
-public class LeaderboardFragment extends Fragment {
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-    private String mParam1;
-    private String mParam2;
-    SwipeRefreshLayout swipeRefreshLayout;
-    RecyclerView recyclerView;
-
-    public LeaderboardFragment() {
-    }
-
-    public static LeaderboardFragment newInstance(String param1, String param2) {
-        LeaderboardFragment fragment = new LeaderboardFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_leaderboard, container, false);
-        recyclerView = root.findViewById(R.id.recyclerView);
-        swipeRefreshLayout = root.findViewById(R.id.swiper);
-
-        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                getLeaderboardData();
-            }
-        });
-
-        return root;
-    }
-
-    private void getLeaderboardData() {
-        swipeRefreshLayout.setRefreshing(false);
-    }
-
-    @Override
-    public void setUserVisibleHint(boolean isVisibleToUser) {
-        super.setUserVisibleHint(isVisibleToUser);
-        if (isVisibleToUser) {
-            getLeaderboardData();
-        }
-    }
-
-
-    //--------LeaderBoardData Adapter-----------
-    public class LeaderBoardAdapter extends RecyclerView.Adapter<LeaderBoardAdapter.MyViewHolder> {
-        Context context;
-
-        public LeaderBoardAdapter(Context context) {
-            this.context = context;
-        }
-
-        @NonNull
-        @Override
-        public LeaderBoardAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.leaderboard_user_ranking_list, parent, false);
-            return new MyViewHolder(view);
-        }
-
-        @Override
-        public void onBindViewHolder(@NonNull LeaderBoardAdapter.MyViewHolder holder, int position) {
-
-        }
-
-        @Override
-        public int getItemCount() {
-            return 0;
-        }
-
-        public class MyViewHolder extends RecyclerView.ViewHolder {
-            TextView userRank, userTotalPoints, userName;
-            ImageView userProfile;
-
-            public MyViewHolder(@NonNull View itemView) {
-                super(itemView);
-                userProfile = itemView.findViewById(R.id.userProfile);
-                userName = itemView.findViewById(R.id.userName);
-                userTotalPoints = itemView.findViewById(R.id.userTotalPoints);
-                userRank = itemView.findViewById(R.id.userRank);
-            }
-        }
-    }
-
-}
+                <category android:name="android.intent.category.LAUNCHER" />
+            </intent-filter>
+        </activity>
+        <activity
+            android:name="com.example.yoyoiq.RegisterAndPlay"
+            android:exported="true" />
+        <activity
+            android:name="com.example.yoyoiq.ContestWinners"
+            android:exported="true" />
+        <activity
+            android:name="com.example.yoyoiq.CustomDialog"
+            android:exported="true" />
+        <activity
+            android:name="com.example.yoyoiq.TeamPreviewActivity"
+            android:exported="true" />
+        <activity
+            android:name="com.example.yoyoiq.ProfileActivity"
+            android:exported="true" />
+        <activity
+            android:name="com.example.yoyoiq.PaymentOptions"
+            android:exported="true" />
+        <activity
+            android:name="com.example.yoyoiq.LoginActivity"
+            android:exported="true" />
+        <activity
+            android:name="com.example.yoyoiq.WalletPackage.RecentTransactions"
+            android:exported="false" />
+        <activity
+            android:name="com.example.yoyoiq.NotificationActivity"
+            android:exported="false" />
+        <activity
+            android:name="com.example.yoyoiq.SettingsActivity"
+            android:exported="true" />
+        <activity
+            android:name="com.example.yoyoiq.WalletPackage.AddCash"
+            android:exported="true" />
+        <activity
+            android:name="com.example.yoyoiq.SelectTeams"
+            android:exported="true" />
+        <activity
+            android:name="com.example.yoyoiq.AddCashActivity"
+            android:exported="false" />
+        <activity
+            android:name="com.example.yoyoiq.MyCoupons"
+            android:exported="false" />
+        <activity
+            android:name="com.example.yoyoiq.FantasyIPL"
+            android:exported="false" />
+        <activity
+            android:name="com.example.yoyoiq.PointSystem"
+            android:exported="false" />
+        <activity
+            android:name="com.example.yoyoiq.MainActivity"
+            android:exported
