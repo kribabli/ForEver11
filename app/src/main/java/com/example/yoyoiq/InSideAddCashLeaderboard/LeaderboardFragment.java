7686af411eeca14be84ec94f371f7a07574010de
Mt Software Solutions <95462784+mtteam21@users.noreply.github.com>
2022-08-27 11:15:05 +0530

    leaderboard_user_ranking_list.xml..

--- app/src/main/java/com/example/yoyoiq/InSideAddCashLeaderboard/LeaderboardFragment.java
+++ app/src/main/java/com/example/yoyoiq/InSideAddCashLeaderboard/LeaderboardFragment.java
@@ -1,10 +1,14 @@
 package com.example.yoyoiq.InSideAddCashLeaderboard;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
@@ -68,4 +72,45 @@ public class LeaderboardFragment extends Fragment {
             getLeaderboardData();
         }
     }
+
+
+    //--------LeaderBoardData Adapter-----------
+    public class LeaderBoardAdapter extends RecyclerView.Adapter<LeaderBoardAdapter.MyViewHolder> {
+        Context context;
+
+        public LeaderBoardAdapter(Context context) {
+            this.context = context;
+        }
+
+        @NonNull
+        @Override
+        public LeaderBoardAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.leaderboard_user_ranking_list, parent, false);
+            return new MyViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull LeaderBoardAdapter.MyViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
+
+        public class MyViewHolder extends RecyclerView.ViewHolder {
+            TextView userRank, userTotalPoints, userName;
+            ImageView userProfile;
+
+            public MyViewHolder(@NonNull View itemView) {
+                super(itemView);
+                userProfile = itemView.findViewById(R.id.userProfile);
+                userName = itemView.findViewById(R.id.userName);
+                userTotalPoints = itemView.findViewById(R.id.userTotalPoints);
+                userRank = itemView.findViewById(R.id.userRank);
+            }
+        }
+    }
+
 }
