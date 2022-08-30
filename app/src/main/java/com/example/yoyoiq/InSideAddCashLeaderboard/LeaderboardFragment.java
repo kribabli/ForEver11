4b961fd3590db37c68d80e7cba1e73f07a9e0cfc
Mt Software Solutions <95462784+mtteam21@users.noreply.github.com>
2022-08-30 14:55:42 +0530

    LeaderboardFragment.

--- app/src/main/java/com/example/yoyoiq/InSideAddCashLeaderboard/LeaderboardFragment.java
+++ app/src/main/java/com/example/yoyoiq/InSideAddCashLeaderboard/LeaderboardFragment.java
@@ -1,95 +1,45 @@
-com.example.yoyoiq.OnlyTeamPreView.OnlyTeamPreview"
-            android:exported="false" />
-        <activity
-            android:name="com.example.yoyoiq.ChatFirebase.ChatActivity"
-            android:exported="true" />
-        <activity
-            android:name="com.example.yoyoiq.ChatFirebase.DirectChatActivity"
-            android:exported="true" />
-        <activity
-            android:name="com.example.yoyoiq.KYC.ShowKYCDetails"
-            android:exported="false" />
-        <activity
-            android:name="com.example.yoyoiq.CreateTeamActivity"
-            android:exported="false" />
-        <activity
-            android:name="com.example.yoyoiq.PrivacyPolicy.AboutUsActivity"
-            android:exported="false" />
-        <activity
-            android:name="com.example.yoyoiq.ContestActivity"
-            android:exported="true" />
-        <activity
-            android:name="com.example.yoyoiq.KYC.KYCActivity"
-            android:exported="false" />
-        <activity
-            android:name="com.example.yoyoiq.RegisterDetails"
-            android:exported="false" />
-        <activity
-            android:name="com.example.yoyoiq.RegisterActivity"
-            android:exported="true" />
-        <activity
-            android:name="com.example.yoyoiq.FrontActivity"
-            android:exported="true" />
-        <activity
-            android:name="com.example.yoyoiq.SearchActivity"
-            android:exported="false" />
-        <activity
-            android:name="com.example.yoyoiq.SplashActivity"
-            android:exported="true" >
-            <intent-filter>
-                <action android:name="android.intent.action.MAIN" />
+package com.example.yoyoiq.InSideAddCashLeaderboard;
 
-                <category android:name="android.intent.category.LAUNCHER" />
-            </intent-filter>
-        </activity>
-        <activity
-            android:name="com.example.yoyoiq.RegisterAndPlay"
-            android:exported="true" />
-        <activity
-            android:name="com.example.yoyoiq.ContestWinners"
-            android:exported="true" />
-        <activity
-            android:name="com.example.yoyoiq.CustomDialog"
-            android:exported="true" />
-        <activity
-            android:name="com.example.yoyoiq.TeamPreviewActivity"
-            android:exported="true" />
-        <activity
-            android:name="com.example.yoyoiq.ProfileActivity"
-            android:exported="true" />
-        <activity
-            android:name="com.example.yoyoiq.PaymentOptions"
-            android:exported="true" />
-        <activity
-            android:name="com.example.yoyoiq.LoginActivity"
-            android:exported="true" />
-        <activity
-            android:name="com.example.yoyoiq.WalletPackage.RecentTransactions"
-            android:exported="false" />
-        <activity
-            android:name="com.example.yoyoiq.NotificationActivity"
-            android:exported="false" />
-        <activity
-            android:name="com.example.yoyoiq.SettingsActivity"
-            android:exported="true" />
-        <activity
-            android:name="com.example.yoyoiq.WalletPackage.AddCash"
-            android:exported="true" />
-        <activity
-            android:name="com.example.yoyoiq.SelectTeams"
-            android:exported="true" />
-        <activity
-            android:name="com.example.yoyoiq.AddCashActivity"
-            android:exported="false" />
-        <activity
-            android:name="com.example.yoyoiq.MyCoupons"
-            android:exported="false" />
-        <activity
-            android:name="com.example.yoyoiq.FantasyIPL"
-            android:exported="false" />
-        <activity
-            android:name="com.example.yoyoiq.PointSystem"
-            android:exported="false" />
-        <activity
-            android:name="com.example.yoyoiq.MainActivity"
-            android:exported
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.yoyoiq.R;
+
+public class LeaderboardFragment extends Fragment {
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    private String mParam1;
+    private String mParam2;
+
+    public LeaderboardFragment() {
+        // Required empty public constructor
+    }
+
+    public static LeaderboardFragment newInstance(String param1, String param2) {
+        LeaderboardFragment fragment = new LeaderboardFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_leaderboard, container, false);
+    }
+}
