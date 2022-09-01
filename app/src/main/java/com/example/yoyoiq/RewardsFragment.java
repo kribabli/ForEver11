2bc6cd159af9213b3b2b6bbfd8324a370e40ebb5
AppSync-ai <81430894+AppSync-ai@users.noreply.github.com>
2022-09-01 19:02:51 +0530

    code commited

--- app/src/main/java/com/example/yoyoiq/RewardsFragment.java
+++ app/src/main/java/com/example/yoyoiq/RewardsFragment.java
@@ -5,72 +5,94 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import com.example.yoyoiq.Adapter.PagerAdapterRewards;
+import com.example.yoyoiq.InSideRewardsFragments.MyRewardsFragment;
+import com.example.yoyoiq.InSideRewardsFragments.RewardShopFragment;
 import com.google.android.material.tabs.TabLayout;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RewardsFragment extends Fragment {
-    ViewPager viewPager;
+
+
     TabLayout tabLayout;
-    PagerAdapterRewards pagerAdapterRewards;
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    ViewPager viewPager;
+    FragmentManager childFragmentManager;
 
-    private String mParam1;
-    private String mParam2;
 
-    public RewardsFragment() {
-    }
-
-    public static RewardsFragment newInstance(String param1, String param2) {
-        RewardsFragment fragment = new RewardsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_rewards, container, false);
-        viewPager = root.findViewById(R.id.viewPager);
-        tabLayout = root.findViewById(R.id.tabLayout);
+        View rootView = inflater.inflate(R.layout.fragment_rewards, container, false);
 
-        pagerAdapterRewards = new PagerAdapterRewards(getFragmentManager(), tabLayout.getTabCount());
-        viewPager.setAdapter(pagerAdapterRewards);
+        viewPager = rootView.findViewById(R.id.viewpager);
+        tabLayout = rootView.findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+        childFragmentManager = getChildFragmentManager();
 
-        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                viewPager.setCurrentItem(tab.getPosition());
-                if (tab.getPosition() == 0) {
-                } else if (tab.getPosition() == 1) {
-                }
-            }
+        for (int i = 0; i < tabLayout.getTabCount(); i++) {
+            View tab = ((ViewGroup) tabLayout.getChildAt(0)).getChildAt(i);
+            ViewGroup.MarginLayoutParams p = (ViewGroup.MarginLayoutParams) tab.getLayoutParams();
+            p.setMargins(20, 0, 20, 0);
+            tab.requestLayout();
+        }
 
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-            }
+        setupViewPager(viewPager);
+        tabLayout.setupWithViewPager(viewPager);
 
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-            }
-        });
-        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
 
-        return root;
+        return rootView;
     }
+
+    private void setupViewPager(ViewPager viewPager) {
+        final ViewPagerAdapter adapter = new ViewPagerAdapter(childFragmentManager);
+        adapter.addFragment(new RewardShopFragment(), "REWARD SHOP");
+        adapter.addFragment(new MyRewardsFragment(), "MY REWARDS");
+        viewPager.setAdapter(adapter);
+
+
+    }
+
+    class ViewPagerAdapter extends FragmentStatePagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        private ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getItemPosition(@NonNull Object object) {
+            return FragmentStatePagerAdapter.POSITION_NONE;
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        private void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+
 }
