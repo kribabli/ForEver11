52491c5e4d812cc7ad8943caba7f77d33c398434
AppSync-ai <81430894+AppSync-ai@users.noreply.github.com>
2022-09-03 00:24:04 +0530

    BannerAdapter.

--- app/src/main/java/com/example/yoyoiq/Adapter/BannerAdapter.java
+++ app/src/main/java/com/example/yoyoiq/Adapter/BannerAdapter.java
@@ -1,6 +1,7 @@
 package com.example.yoyoiq.Adapter;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +10,7 @@ import android.widget.ImageView;
 import androidx.annotation.NonNull;
 import androidx.viewpager.widget.PagerAdapter;
 
+import com.bumptech.glide.Glide;
 import com.example.yoyoiq.Model.The_Slide_Items_Model_Class;
 import com.example.yoyoiq.R;
 
@@ -17,21 +19,25 @@ import java.util.List;
 public class BannerAdapter extends PagerAdapter {
     Context context;
     List<The_Slide_Items_Model_Class> bannerListImages;
+    private LayoutInflater inflater;
     //This Adapter is use for Banner
 
     public BannerAdapter(Context context, List<The_Slide_Items_Model_Class> bannerListImages) {
         this.context = context;
         this.bannerListImages = bannerListImages;
+        this.inflater = inflater;
     }
 
     @NonNull
     @Override
     public Object instantiateItem(@NonNull ViewGroup container, int position) {
-
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View sliderLayout = inflater.inflate(R.layout.banner_layout, null);
         ImageView allSliderBannerImages = sliderLayout.findViewById(R.id.allSliderBannerImages);
-        allSliderBannerImages.setImageResource(bannerListImages.get(position).getUri());
+        Glide.with(context)
+                .load(bannerListImages.get(position).getUri())
+                .into(allSliderBannerImages);
+
         container.addView(sliderLayout);
         return sliderLayout;
     }
