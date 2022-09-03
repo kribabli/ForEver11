ecc4870ad52f47698b74b306aeaa02cfa9739d9b
Babli Kumari <skribabli@gmail.com>
2022-09-04 01:02:35 +0530

    onlyteam_preview_list.xml..

--- app/src/main/java/com/example/yoyoiq/HomeFragment.java
+++ app/src/main/java/com/example/yoyoiq/HomeFragment.java
@@ -5,8 +5,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 
@@ -24,108 +27,77 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class HomeFragment extends Fragment {
-    RecyclerView recyclerView;
-    ViewPager view_bannerItem;
     TabLayout tabLayout;
-    View myFragment;
-    ArrayList<The_Slide_Items_Model_Class> listItems = new ArrayList<>();
-
-    public HomeFragment() {
-    }
-
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setAutoSliderBanner();
-        if (getArguments() != null) {
-        }
-    }
-
-    BannerAdapter bannerAdapter;
+    ViewPager viewPager;
+    FragmentManager childFragmentManager;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        myFragment = inflater.inflate(R.layout.fragment_home, container, false);
-        recyclerView = myFragment.findViewById(R.id.recyclerViewMatchList);
-        view_bannerItem = myFragment.findViewById(R.id.view_bannerItem);
-        tabLayout = myFragment.findViewById(R.id.tabLayout);
-        return myFragment;
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        viewPager = rootView.findViewById(R.id.viewpager);
+        tabLayout = rootView.findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+        childFragmentManager = getChildFragmentManager();
+
+        for (int i = 0; i < tabLayout.getTabCount(); i++) {
+            View tab = ((ViewGroup) tabLayout.getChildAt(0)).getChildAt(i);
+            ViewGroup.MarginLayoutParams p = (ViewGroup.MarginLayoutParams) tab.getLayoutParams();
+            p.setMargins(20, 0, 20, 0);
+            tab.requestLayout();
+        }
+
+        setupViewPager(viewPager);
+        tabLayout.setupWithViewPager(viewPager);
+
+        return rootView;
     }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        setUpViewPager(view_bannerItem);
-        tabLayout.setupWithViewPager(view_bannerItem);
-        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
+    private void setupViewPager(ViewPager viewPager) {
+        final ViewPagerAdapter adapter = new ViewPagerAdapter(childFragmentManager);
+        adapter.addFragment(new CricketFragment(), "CRICKET");
+        viewPager.setAdapter(adapter);
     }
 
-    private void setUpViewPager(ViewPager view_bannerItem) {
-        FragmentAdapter fragmentAdapter = new FragmentAdapter(getChildFragmentManager());
-        fragmentAdapter.addFragment(new CricketFragment(), "Cricket");
-        view_bannerItem.setAdapter(fragmentAdapter);
+    class ViewPagerAdapter extends FragmentStatePagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        private ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getItemPosition(@NonNull Object object) {
+            return FragmentStatePagerAdapter.POSITION_NONE;
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        private void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
     }
 
-    private void setAutoSliderBanner() {
-        Call<Banner> call = ApiClient.getInstance().getApi().getBanner();
-        call.enqueue(new Callback<Banner>() {
-            @Override
-            public void onResponse(Call<Banner> call, Response<Banner> response) {
-                Banner banner = response.body();
-                if (response.isSuccessful()) {
-                    String bannerData = new Gson().toJson(banner.getResponse());
-                    try {
-                        JSONArray jsonArray = new JSONArray(bannerData);
-                        for (int i = 0; i < jsonArray.length(); i++) {
-                            JSONObject jsonObject = jsonArray.getJSONObject(i);
-                            String id = jsonObject.getString("id");
-                            String image = jsonObject.getString("image");
-                            The_Slide_Items_Model_Class the_slide_items_model_class = new The_Slide_Items_Model_Class(image);
-                            listItems.add(the_slide_items_model_class);
-                            bannerAdapter = new BannerAdapter(getContext(), listItems);
-                            view_bannerItem.setAdapter(bannerAdapter);
-                            bannerAdapter.notifyDataSetChanged();
-                        }
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                } else {
-                }
-            }
-
-            @Override
-            public void onFailure(Call<Banner> call, Throwable t) {
-
-            }
-        });
-    }
 }
