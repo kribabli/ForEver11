9c2187775d3ac8b1c421c8ea469e62c0156317ac
Mt Software Solutions <95462784+mtteam21@users.noreply.github.com>
2022-09-07 16:19:42 +0530

    ProfileAdapter.java.

--- app/src/main/java/com/example/yoyoiq/InSideAddCashLeaderboard/LeaderboardFragment.java
+++ app/src/main/java/com/example/yoyoiq/InSideAddCashLeaderboard/LeaderboardFragment.java
@@ -1,19 +1,30 @@
 package com.example.yoyoiq.InSideAddCashLeaderboard;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.yoyoiq.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class LeaderboardFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
     private String mParam1;
     private String mParam2;
+    String url = "https://adminapp.tech/yoyoiq/ItsMe/all_apis.php?func=get_leaderboard_users";
 
     public LeaderboardFragment() {
         // Required empty public constructor
@@ -31,6 +42,11 @@ public class LeaderboardFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        try {
+            getLeaderBoardData("53334","227");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -41,5 +57,32 @@ public class LeaderboardFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_leaderboard, container, false);
+    }
+
+    private void getLeaderBoardData(String match_id, String contest_id) throws JSONException {
+        RequestQueue queue = Volley.newRequestQueue(getContext());
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("match_id", 53334);
+        jsonObject.put("contest_id", 227);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, jsonObject,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("TAG", "onResponse: "+response);
+                        try {
+                            if (response.getString("Result").equalsIgnoreCase("true")) {
+                                JSONObject jsonObject1 = new JSONObject(String.valueOf(response.getJSONObject("UserLogin")));
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        });
+        queue.add(jsonObjectRequest);
     }
 }
