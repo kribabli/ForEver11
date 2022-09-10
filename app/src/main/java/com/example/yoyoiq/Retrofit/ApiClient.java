7edb6c6489c085092f35ee8218492665047dbd4d
Mt Software Solutions <95462784+mtteam21@users.noreply.github.com>
2022-09-10 12:03:52 +0530

    ApiClient.

--- app/src/main/java/com/example/yoyoiq/Retrofit/ApiClient.java
+++ app/src/main/java/com/example/yoyoiq/Retrofit/ApiClient.java
@@ -4,8 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    //    public static String BASE_URL = "http://adminapp.tech/forever11/api/";
-    public static String BASE_URL = "http://adminapp.tech/yoyoiq/api/";
+    public static String BASE_URL = "http://adminapp.tech/forever11/api/";
     private static ApiClient apiClient;
     private static Retrofit retrofit;
 
--- app/src/main/java/com/example/yoyoiq/SettingsActivity.java
+++ app/src/main/java/com/example/yoyoiq/SettingsActivity.java
@@ -2,22 +2,37 @@ package com.example.yoyoiq;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.yoyoiq.KYC.ViewKycResponse;
+import com.example.yoyoiq.Retrofit.ApiClient;
 import com.example.yoyoiq.common.HelperData;
 import com.example.yoyoiq.common.SessionManager;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class SettingsActivity extends AppCompatActivity {
-    TextView backPress, userName, email, mobileNo, Country, logOut;
+    TextView backPress, userName, email, mobileNo, Country, logOut, city;
     SessionManager sessionManager;
     GoogleSignInOptions gso;
     GoogleSignInClient gsc;
+    boolean status = false;
+    String dob, address;
+    Button DOB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +40,7 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
         initMethod();
         setAction();
+        getUserDetailFromAPI();
         upDateProfile();
     }
 
@@ -34,6 +50,8 @@ public class SettingsActivity extends AppCompatActivity {
         email = findViewById(R.id.email);
         mobileNo = findViewById(R.id.mobileNo);
         Country = findViewById(R.id.Country);
+        city = findViewById(R.id.city);
+        DOB = findViewById(R.id.date_of_birth);
         logOut = findViewById(R.id.logOut);
         gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
         gsc = GoogleSignIn.getClient(this, gso);
@@ -46,10 +64,45 @@ public class SettingsActivity extends AppCompatActivity {
         logOut.setOnClickListener(v -> userLogout());
     }
 
+    private void getUserDetailFromAPI() {
+        Call<ViewKycResponse> call = ApiClient.getInstance().getApi().getkycDetails(sessionManager.getUserData().getUser_id());
+        call.enqueue(new Callback<ViewKycResponse>() {
+            @Override
+            public void onResponse(Call<ViewKycResponse> call, Response<ViewKycResponse> response) {
+                ViewKycResponse viewKycResponse = response.body();
+                if (response.isSuccessful()) {
+                    String data = new Gson().toJson(viewKycResponse.isStatus());
+                    status = Boolean.parseBoolean(data);
+                    JSONArray jsonArray1 = null;
+                    if (status != false) {
+                        String data2 = new Gson().toJson(viewKycResponse.getKycDetails());
+                        try {
+                            jsonArray1 = new JSONArray(data2);
+                            for (int i = 0; i < jsonArray1.length(); i++) {
+                                JSONObject jsonObject = jsonArray1.getJSONObject(i);
+                                dob = jsonObject.getString("dob");
+                                address = jsonObject.getString("address");
+                                String status = jsonObject.getString("status");
+                            }
+                            city.setText(address);
+                            DOB.setText(dob);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ViewKycResponse> call, Throwable t) {
+            }
+        });
+    }
+
     private void upDateProfile() {
-        userName.setText(HelperData.UserName);
-        email.setText(HelperData.UserEmail);
-        mobileNo.setText(HelperData.Usermobile);
+        userName.setText(sessionManager.getUserData().getUserName());
+        email.setText(sessionManager.getUserData().getEmailId());
+        mobileNo.setText(sessionManager.getUserData().getMobileNo());
         Country.setText("India");
     }
 
