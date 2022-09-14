d78a977197273d66b934733c8a1b4958b987ac10
Mt Software Solutions <95462784+mtteam21@users.noreply.github.com>
2022-09-14 18:42:53 +0530

    squadsA.

--- app/src/main/java/com/example/yoyoiq/PlayerPOJO/squadsA.java
+++ app/src/main/java/com/example/yoyoiq/PlayerPOJO/squadsA.java
@@ -7,14 +7,16 @@ public class squadsA {
     String role_str;
     String playing11;
     String name;
+    int avg_points;
 
-    public squadsA(String player_id, String role, String substitute, String role_str, String playing11, String name) {
+    public squadsA(String player_id, String role, String substitute, String role_str, String playing11, String name,int avg_points) {
         this.player_id = player_id;
         this.role = role;
         this.substitute = substitute;
         this.role_str = role_str;
         this.playing11 = playing11;
         this.name = name;
+        this.avg_points = avg_points;
     }
 
     public String getPlayer_id() {
@@ -63,5 +65,13 @@ public class squadsA {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getAvg_points() {
+        return avg_points;
+    }
+
+    public void setAvg_points(int avg_points) {
+        this.avg_points = avg_points;
     }
 }
