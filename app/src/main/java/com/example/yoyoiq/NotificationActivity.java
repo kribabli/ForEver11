c1693fc49e8af41caa52efb51f3ec4e81951ab54
AppSync-ai <81430894+AppSync-ai@users.noreply.github.com>
2022-10-14 17:39:43 +0530

    NotificationActivity.java

--- app/src/main/java/com/example/yoyoiq/NotificationActivity.java
+++ app/src/main/java/com/example/yoyoiq/NotificationActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -39,7 +38,6 @@ public class NotificationActivity extends AppCompatActivity {
                             return;
                         }
                         String token = task.getResult();
-                        Toast.makeText(NotificationActivity.this, token, Toast.LENGTH_SHORT).show();
                         editText.setText(token);
                     }
                 });
